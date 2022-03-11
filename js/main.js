78d0649e7c6fc4b8e76a22e54c93d6a8ed9f0910
Rapkowsky <robertrapkowski19@gmail.com>
2022-03-11 20:14:46 +0100

    Basic file structure added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const incomeSection = document.querySelector(".income-area");
+const expensesSection = document.querySelector(".expenses-area");
+const availableMoney = document.querySelector(".available-money");
+const addTransactionPanel = document.querySelector(".add-transaction-panel");
+
+const nameInput = document.querySelector("#name");
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#category");
+
+const addTransactionBtn = document.querySelector(".add-transaction");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+const deleteAllBtn = document.querySelector(".delete-all");
+
+const lightStyleBtn = document.querySelector(".light");
+const darkStyleBtn = document.querySelector(".dark");
